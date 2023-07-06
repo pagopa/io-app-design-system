560ebe1e5d48c8aa06184aae13804315aa8d4219
Davide Marro <davide.marro@pagopa.it>
2023-07-06 10:33:18 +0200

    Migrating components from io-app

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,164 @@
+import React, { useCallback } from "react";
+import { GestureResponderEvent, Pressable } from "react-native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  interpolateColor,
+  useAnimatedProps,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring
+} from "react-native-reanimated";
+import { IOButtonStyles, IOColors, IOIconButtonStyles, IOScaleValues, IOSpringValues, hexToRgba } from "../../core";
+import { WithTestID } from "../../utils/types";
+import { AnimatedIcon, IOIcons, IconClassComponent } from "../icons";
+
+export type IconButton = WithTestID<{
+  color?: "primary" | "neutral" | "contrast";
+  icon: IOIcons;
+  disabled?: boolean;
+  accessibilityLabel: string;
+  accessibilityHint?: string;
+  onPress: (event: GestureResponderEvent) => void;
+}>;
+
+type ColorStates = {
+  icon: {
+    default: string;
+    pressed: string;
+    disabled: string;
+  };
+};
+
+const mapColorStates: Record<NonNullable<IconButton["color"]>, ColorStates> = {
+  // Primary button
+  primary: {
+    icon: {
+      default: IOColors["blueIO-500"],
+      pressed: IOColors["blueIO-600"],
+      disabled: hexToRgba(IOColors["blueIO-500"], 0.25)
+    }
+  },
+  // Neutral button
+  neutral: {
+    icon: {
+      default: IOColors.black,
+      pressed: IOColors["grey-850"],
+      disabled: IOColors.grey
+    }
+  },
+  // Contrast button
+  contrast: {
+    icon: {
+      default: IOColors.white,
+      pressed: hexToRgba(IOColors.white, 0.85),
+      disabled: hexToRgba(IOColors.white, 0.25)
+    }
+  }
+};
+
+const AnimatedIconClassComponent =
+  Animated.createAnimatedComponent(IconClassComponent);
+
+export const IconButton = ({
+  color = "primary",
+  icon,
+  disabled = false,
+  onPress,
+  accessibilityLabel,
+  accessibilityHint,
+  testID
+}: IconButton) => {
+  const isPressed = useSharedValue(0);
+
+  // Scaling transformation applied when the button is pressed
+  const animationScaleValue = IOScaleValues?.exaggeratedButton?.pressedState;
+
+  // Using a spring-based animation for our interpolations
+  const progressPressed = useDerivedValue(() =>
+    withSpring(isPressed.value, IOSpringValues.button)
+  );
+
+  // Interpolate animation values from `isPressed` values
+
+  const pressedAnimationStyle = useAnimatedStyle(() => {
+    // Scale down button slightly when pressed
+    const scale = interpolate(
+      progressPressed.value,
+      [0, 1],
+      [1, animationScaleValue],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      transform: [{ scale }]
+    };
+  });
+
+  // Animate the <Icon> color prop
+  const animatedColor = useAnimatedProps(() => {
+    const iconColor = interpolateColor(
+      progressPressed.value,
+      [0, 1],
+      [
+        mapColorStates[color].icon.default,
+        mapColorStates[color].icon.pressed
+      ]
+    );
+    return { color: iconColor };
+  });
+  /* REMOVE_LEGACY_COMPONENT: End ▶ */
+
+  const handlePressIn = useCallback(() => {
+    // eslint-disable-next-line functional/immutable-data
+    isPressed.value = 1;
+  }, [isPressed]);
+  const handlePressOut = useCallback(() => {
+    // eslint-disable-next-line functional/immutable-data
+    isPressed.value = 0;
+  }, [isPressed]);
+
+  return (
+    <Pressable
+      style={IOButtonStyles.dimensionsDefault}
+      disabled={disabled}
+      // Events
+      onPress={onPress}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      // Accessibility
+      accessible={true}
+      accessibilityRole={"button"}
+      accessibilityLabel={accessibilityLabel}
+      accessibilityHint={accessibilityHint}
+      // Usability
+      // Add a touchable area around the button
+      hitSlop={8}
+      // Test
+      testID={testID}
+    >
+      <Animated.View
+        style={[
+          IOIconButtonStyles.buttonSizeSmall,
+          !disabled && pressedAnimationStyle
+        ]}
+      >
+        {!disabled ? (
+          <AnimatedIconClassComponent
+            name={icon}
+            animatedProps={animatedColor}
+            color={mapColorStates[color]?.icon?.default}
+          />
+        ) : (
+          <AnimatedIcon
+            name={icon}
+            color={mapColorStates[color]?.icon?.disabled}
+          />
+        )}
+      </Animated.View>
+    </Pressable>
+  );
+};
+
+export default IconButton;
--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -2,3 +2,4 @@ export * from "./ButtonSolid";
 export * from "./ButtonLink";
 export * from "./ButtonOutline";
 export * from "./ButtonExtendedOutline";
+export * from "./IconButton";
