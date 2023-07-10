9523e1d85e80db2802cffe67a963492c3bbc0bd5
Cristiano Tofani <cri.tofani@gmail.com>
2023-07-10 15:54:58 +0200

    Adds sample config for react-navigation

---
+++ example/src/pages/MainScreen.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { IOStyles } from "@pagopa/io-app-design-system";
+
+const MainScreen = () => (
+  <View style={IOStyles.flex}>
+    <Text>Result: {0}</Text>
+  </View>
+);
+
+export default MainScreen;
