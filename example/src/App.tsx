acdcefb74f5548f32fc2dd00f7f300382d6d3462
Cristiano Tofani <cri.tofani@gmail.com>
2023-07-10 16:19:18 +0200

    Setup navigation architecture of the example showroom

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
 import "react-native-gesture-handler";
 import { IOStyles } from "@pagopa/io-app-design-system";
-import AppNavigator from "./navigator";
+import AppNavigator from "./navigation/navigator";
 
 export default function App() {
   return (
