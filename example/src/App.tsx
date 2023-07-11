a26d0b005c899e5c0d7174eaf0e578a746880389
Davide Marro <davide.marro@pagopa.it>
2023-07-11 16:49:44 +0200

    Adding fonts

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,17 @@
-import * as React from "react";
+import { IOStyles } from "@pagopa/io-app-design-system";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from 'expo-font';
+import * as React from "react";
 import { SafeAreaView } from "react-native";
 import "react-native-gesture-handler";
-import { IOStyles } from "@pagopa/io-app-design-system";
 import AppNavigator from "./navigation/navigator";
 
 export default function App() {
+  useFonts({
+    "Titillium Web": require("../assets/fonts/TitilliumWeb/TitilliumWeb-Regular.ttf"),
+    "Readex Pro": require("../assets/fonts/ReadexPro/ReadexPro-Regular.ttf"),
+  });
+
   return (
     <SafeAreaView style={IOStyles.flex}>
       <NavigationContainer>
