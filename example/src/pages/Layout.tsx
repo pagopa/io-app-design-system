fdb861b0a48102c7d2f6ee19c4a4120ccc46caa5
Davide Marro <davide.marro@pagopa.it>
2023-07-21 10:12:21 +0200

    Applying prettier

--- example/src/pages/Layout.tsx
+++ example/src/pages/Layout.tsx
@@ -1,155 +1,164 @@
-import { Body, ContentWrapper, Divider, H1, H3, HSpacer, IOAppMargin, IOColors, IOSpacer, IOThemeContext, LabelSmall, VDivider, VSpacer } from "@pagopa/io-app-design-system";
+import {
+    Body,
+    ContentWrapper,
+    Divider,
+    H1,
+    H3,
+    HSpacer,
+    IOAppMargin,
+    IOColors,
+    IOSpacer,
+    IOThemeContext,
+    LabelSmall,
+    VDivider,
+    VSpacer
+} from "@pagopa/io-app-design-system";
 import React, { useContext } from "react";
 import { View } from "react-native";
 import { NoMarginScreen } from "../components/Screen";
 import { SpacerViewerBox } from "../components/SpacerViewerBox";
 
 export const Layout = () => {
-    const theme = useContext(IOThemeContext);
-    
-    return (
-        <NoMarginScreen>
-            <ContentWrapper>
-                <H1
-                    color={theme["textHeading-default"]}
-                    weight={"Bold"}
-                    style={{ marginBottom: 16 }}
+  const theme = useContext(IOThemeContext);
+
+  return (
+    <NoMarginScreen>
+      <ContentWrapper>
+        <H1
+          color={theme["textHeading-default"]}
+          weight={"Bold"}
+          style={{ marginBottom: 16 }}
+        >
+          Grid
+        </H1>
+        <H3
+          color={theme["textHeading-default"]}
+          weight={"SemiBold"}
+          style={{ marginBottom: 16 }}
+        >
+          ContentWrapper
+        </H3>
+      </ContentWrapper>
+      {IOAppMargin.map((value, i, arr) => (
+        <React.Fragment key={`${value}-${i}`}>
+          <View
+            style={{
+              backgroundColor: IOColors[theme["appBackground-tertiary"]]
+            }}
+          >
+            <ContentWrapper margin={value}>
+              <View
+                style={{
+                  paddingVertical: 16,
+                  backgroundColor: IOColors[theme["appBackground-secondary"]]
+                }}
+              >
+                <Body color={theme["textBody-secondary"]}>Content example</Body>
+                <LabelSmall
+                  style={{ position: "absolute", right: 4, top: 4 }}
+                  fontSize="small"
+                  weight="Regular"
+                  color={theme["textBody-tertiary"]}
                 >
-                    Grid
-                </H1>
-                <H3
-                    color={theme["textHeading-default"]}
-                    weight={"SemiBold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    ContentWrapper
-                </H3>
+                  {value}
+                </LabelSmall>
+              </View>
             </ContentWrapper>
-            {IOAppMargin.map((value, i, arr) => (
-                <React.Fragment key={`${value}-${i}`}>
-                    <View
-                        style={{
-                            backgroundColor: IOColors[theme["appBackground-tertiary"]]
-                        }}
-                    >
-                        <ContentWrapper margin={value}>
-                            <View
-                                style={{
-                                    paddingVertical: 16,
-                                    backgroundColor: IOColors[theme["appBackground-secondary"]]
-                                }}
-                            >
-                                <Body color={theme["textBody-secondary"]}>
-                                    Content example
-                                </Body>
-                                <LabelSmall
-                                    style={{ position: "absolute", right: 4, top: 4 }}
-                                    fontSize="small"
-                                    weight="Regular"
-                                    color={theme["textBody-tertiary"]}
-                                >
-                                    {value}
-                                </LabelSmall>
-                            </View>
-                        </ContentWrapper>
-                    </View>
-                    {i !== arr.length - 1 && <VSpacer size={16} />}
-                </React.Fragment>
-            ))}
+          </View>
+          {i !== arr.length - 1 && <VSpacer size={16} />}
+        </React.Fragment>
+      ))}
 
-            <VSpacer size={40} />
+      <VSpacer size={40} />
 
-            <ContentWrapper>
-                <H1
-                    color={theme["textHeading-default"]}
-                    weight={"Bold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    Spacing
-                </H1>
+      <ContentWrapper>
+        <H1
+          color={theme["textHeading-default"]}
+          weight={"Bold"}
+          style={{ marginBottom: 16 }}
+        >
+          Spacing
+        </H1>
 
-                <H3
-                    color={theme["textHeading-default"]}
-                    weight={"SemiBold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    VSpacer
-                </H3>
+        <H3
+          color={theme["textHeading-default"]}
+          weight={"SemiBold"}
+          style={{ marginBottom: 16 }}
+        >
+          VSpacer
+        </H3>
 
-                {/* Vertical */}
-                {IOSpacer.map((spacerEntry, i, arr) => (
-                    <React.Fragment key={`${spacerEntry}-${i}-vertical`}>
-                        <SpacerViewerBox orientation="vertical" size={spacerEntry} />
-                        {/* Don't add spacer to the last item. Quick and dirty
+        {/* Vertical */}
+        {IOSpacer.map((spacerEntry, i, arr) => (
+          <React.Fragment key={`${spacerEntry}-${i}-vertical`}>
+            <SpacerViewerBox orientation="vertical" size={spacerEntry} />
+            {/* Don't add spacer to the last item. Quick and dirty
         alternative to the Stack component.
         https://stackoverflow.com/a/60975451 */}
-                        {i !== arr.length - 1 && <VSpacer size={16} />}
-                    </React.Fragment>
-                ))}
+            {i !== arr.length - 1 && <VSpacer size={16} />}
+          </React.Fragment>
+        ))}
 
-                <VSpacer size={24} />
+        <VSpacer size={24} />
 
-                <H3
-                    color={theme["textHeading-default"]}
-                    weight={"SemiBold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    HSpacer
-                </H3>
+        <H3
+          color={theme["textHeading-default"]}
+          weight={"SemiBold"}
+          style={{ marginBottom: 16 }}
+        >
+          HSpacer
+        </H3>
 
-                {/* Horizontal */}
-                <View style={{ flexDirection: "row" }}>
-                    {IOSpacer.map((spacerEntry, i, arr) => (
-                        <React.Fragment key={`${spacerEntry}-${i}-horizontal`}>
-                            <SpacerViewerBox
-                                orientation="horizontal"
-                                size={spacerEntry}
-                            />
-                            {i !== arr.length - 1 && <HSpacer size={8} />}
-                        </React.Fragment>
-                    ))}
-                </View>
+        {/* Horizontal */}
+        <View style={{ flexDirection: "row" }}>
+          {IOSpacer.map((spacerEntry, i, arr) => (
+            <React.Fragment key={`${spacerEntry}-${i}-horizontal`}>
+              <SpacerViewerBox orientation="horizontal" size={spacerEntry} />
+              {i !== arr.length - 1 && <HSpacer size={8} />}
+            </React.Fragment>
+          ))}
+        </View>
 
-                <VSpacer size={48} />
-            </ContentWrapper>
+        <VSpacer size={48} />
+      </ContentWrapper>
 
-            <ContentWrapper>
-                <H1
-                    color={theme["textHeading-default"]}
-                    weight={"Bold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    Divider
-                </H1>
+      <ContentWrapper>
+        <H1
+          color={theme["textHeading-default"]}
+          weight={"Bold"}
+          style={{ marginBottom: 16 }}
+        >
+          Divider
+        </H1>
 
-                <H3
-                    color={theme["textHeading-default"]}
-                    weight={"SemiBold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    Default (Horizontal)
-                </H3>
+        <H3
+          color={theme["textHeading-default"]}
+          weight={"SemiBold"}
+          style={{ marginBottom: 16 }}
+        >
+          Default (Horizontal)
+        </H3>
 
-                <Divider />
-                <VSpacer size={48} />
-            </ContentWrapper>
-            <Divider />
-            <VSpacer size={48} />
+        <Divider />
+        <VSpacer size={48} />
+      </ContentWrapper>
+      <Divider />
+      <VSpacer size={48} />
 
-            <ContentWrapper>
-                <H3
-                    color={theme["textHeading-default"]}
-                    weight={"SemiBold"}
-                    style={{ marginBottom: 16 }}
-                >
-                    Vertical
-                </H3>
+      <ContentWrapper>
+        <H3
+          color={theme["textHeading-default"]}
+          weight={"SemiBold"}
+          style={{ marginBottom: 16 }}
+        >
+          Vertical
+        </H3>
 
-                <View style={{ flexDirection: "row", height: 100 }}>
-                    <VDivider />
-                </View>
-                <VSpacer size={48} />
-            </ContentWrapper>
-        </NoMarginScreen>
-    );
+        <View style={{ flexDirection: "row", height: 100 }}>
+          <VDivider />
+        </View>
+        <VSpacer size={48} />
+      </ContentWrapper>
+    </NoMarginScreen>
+  );
 };
