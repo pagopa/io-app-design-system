523b986ece52eebe69ca1bcabc78fb0730c25a51
Cristiano Tofani <cri.tofani@gmail.com>
2023-08-03 17:18:51 +0200

    Fixes

--- example/index.js
+++ example/index.js
@@ -1,7 +1,6 @@
 /**
  * @format
  */
-import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import App from "./src/App";
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,15 @@
 import { IOStyles } from "@pagopa/io-app-design-system";
 import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
-import { SafeAreaView } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./navigation/navigator";
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView style={IOStyles.flex}>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </SafeAreaView>
+    <GestureHandlerRootView style={IOStyles.flex}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
