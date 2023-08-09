2dc7cfe72f5c15e8b808706b7a3b1fff9387d7dd
Damiano Plebani <damiano.plebani@pagopa.it>
2023-08-09 14:10:47 +0200

    Add separate `renderRawAccordion` function

--- example/src/pages/Accordion.tsx
+++ example/src/pages/Accordion.tsx
@@ -76,46 +76,52 @@ export const Accordion = () => {
         renderItem={renderItem}
       />
 
-      <VSpacer size={40} />
-      <H3>RawAccordion</H3>
-      <VSpacer size={16} />
-      <View style={[IOStyles.flex, { width: "100%" }]}>
-        <RawAccordion
-          headerStyle={{
-            paddingVertical: 16,
-            backgroundColor: IOColors.greyLight
-          }}
-          header={
-            <View style={IOStyles.row}>
-              <Icon name="productIOApp" size={24} color="grey-650" />
-              <H3 style={{ alignSelf: "center" }}>{"Custom header "}</H3>
-              <H5 style={{ alignSelf: "center" }}>{"Purgatorio, Canto VI"}</H5>
-            </View>
-          }
-        >
-          <Body>
-            {"Ahi serva Italia, di dolore ostello, \n" +
-              "nave sanza nocchiere in gran tempesta, \n" +
-              "non donna di province, ma bordello!" +
-              "\n\n" +
-              "Quell’anima gentil fu così presta, \n" +
-              "sol per lo dolce suon de la sua terra, \n" +
-              "di fare al cittadin suo quivi festa;" +
-              "\n\n" +
-              "e ora in te non stanno sanza guerra \n" +
-              "li vivi tuoi, e l’un l’altro si rode \n" +
-              "di quei ch’un muro e una fossa serra." +
-              "\n\n" +
-              "Cerca, misera, intorno da le prode \n" +
-              "le tue marine, e poi ti guarda in seno, \n" +
-              "s’alcuna parte in te di pace gode." +
-              "\n\n" +
-              "Che val perché ti racconciasse il freno \n" +
-              "Iustiniano, se la sella è vota? \n" +
-              "Sanz’esso fora la vergogna meno."}
-          </Body>
-        </RawAccordion>
-      </View>
+      {renderRawAccordion()}
     </Screen>
   );
 };
+
+const renderRawAccordion = () => (
+  <>
+    <VSpacer size={40} />
+    <H3>RawAccordion</H3>
+    <VSpacer size={16} />
+    <View style={[IOStyles.flex, { width: "100%" }]}>
+      <RawAccordion
+        headerStyle={{
+          paddingVertical: 16,
+          backgroundColor: IOColors.greyLight
+        }}
+        header={
+          <View style={IOStyles.row}>
+            <Icon name="productIOApp" size={24} color="grey-650" />
+            <H3 style={{ alignSelf: "center" }}>{"Custom header "}</H3>
+            <H5 style={{ alignSelf: "center" }}>{"Purgatorio, Canto VI"}</H5>
+          </View>
+        }
+      >
+        <Body>
+          {"Ahi serva Italia, di dolore ostello, \n" +
+            "nave sanza nocchiere in gran tempesta, \n" +
+            "non donna di province, ma bordello!" +
+            "\n\n" +
+            "Quell’anima gentil fu così presta, \n" +
+            "sol per lo dolce suon de la sua terra, \n" +
+            "di fare al cittadin suo quivi festa;" +
+            "\n\n" +
+            "e ora in te non stanno sanza guerra \n" +
+            "li vivi tuoi, e l’un l’altro si rode \n" +
+            "di quei ch’un muro e una fossa serra." +
+            "\n\n" +
+            "Cerca, misera, intorno da le prode \n" +
+            "le tue marine, e poi ti guarda in seno, \n" +
+            "s’alcuna parte in te di pace gode." +
+            "\n\n" +
+            "Che val perché ti racconciasse il freno \n" +
+            "Iustiniano, se la sella è vota? \n" +
+            "Sanz’esso fora la vergogna meno."}
+        </Body>
+      </RawAccordion>
+    </View>
+  </>
+);
