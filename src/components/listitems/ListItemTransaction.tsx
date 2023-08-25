b89df80d660d05b205bca504960ef9c3a287163c
Alessandro Izzo <alessandro.izzo@pagopa.it>
2023-08-25 10:19:17 +0200

    chore: Change title typography and num of lines to 2

--- src/components/listitems/ListItemTransaction.tsx
+++ src/components/listitems/ListItemTransaction.tsx
@@ -17,7 +17,7 @@ import { Badge } from "../badge/Badge";
 import { Icon } from "../icons";
 import { IOLogoPaymentType, LogoPayment } from "../logos";
 import { VSpacer } from "../spacer";
-import { H6, LabelSmall } from "../typography";
+import { H5, H6, LabelSmall } from "../typography";
 import {
   PressableBaseProps,
   PressableListItemBase
@@ -115,9 +115,7 @@ export const ListItemTransaction = ({
           </View>
         )}
         <View style={IOStyles.flex}>
-          <H6 numberOfLines={1} color={theme["textBody-default"]}>
-            {title}
-          </H6>
+          <H5 numberOfLines={2}>{title}</H5>
           <LabelSmall weight="Regular" color={theme["textBody-tertiary"]}>
             {subtitle}
           </LabelSmall>
