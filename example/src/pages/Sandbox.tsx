0b500d27d568f1a9391ef7f3f02855ec4018ae32
Cristiano Tofani <cri.tofani@gmail.com>
2023-08-30 17:41:36 +0200

    revert sandbox component

--- example/src/pages/Sandbox.tsx
+++ example/src/pages/Sandbox.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
-import { SafeAreaView, View } from "react-native";
+import { View } from "react-native";
 import {
   H1,
   H5,
   IOVisualCostants,
   IOStyles,
-  VSpacer,
-  FooterWithButtons
+  VSpacer
 } from "@pagopa/io-app-design-system";
-import { constVoid } from "fp-ts/lib/function";
 import { Screen } from "../components/Screen";
 
 /**
@@ -16,30 +14,14 @@ import { Screen } from "../components/Screen";
  * @returns a screen with a flexed view where you can test components
  */
 export const Sandbox = () => (
-  <SafeAreaView style={IOStyles.flex}>
-    <View style={{ flexGrow: 1 }}>
-      <FooterWithButtons
-        primary={{
-          type: "Solid",
-          buttonProps: {
-            color: "primary",
-            accessibilityLabel: "primary button",
-            onPress: constVoid,
-            label: "Primary button"
-          }
-        }}
-        secondary={{
-          type: "Outline",
-          buttonProps: {
-            color: "primary",
-            fullWidth: true,
-            accessibilityLabel: "secondary button",
-            onPress: constVoid,
-            label: "Secondary button"
-          }
-        }}
-        type="TwoButtonsInlineHalf"
-      />
+  <Screen>
+    <View
+      style={[IOStyles.flex, { paddingTop: IOVisualCostants.appMarginDefault }]}
+    >
+      <H1>Sandbox</H1>
+      <H5>{"Insert here the component you're willing to test"}</H5>
+      <VSpacer />
+      {/* Insert here the component you're willing to test */}
     </View>
-  </SafeAreaView>
+  </Screen>
 );
