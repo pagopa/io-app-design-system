6c2c0a3d9796de61fec2a51480ee4c25c4db0a4b
Cristiano Tofani <cri.tofani@gmail.com>
2023-09-01 11:49:32 +0200

    [IOPLT-112] Adds the FooterWithButtons component (#53)
    ## Short description
    This PR creates a new version of the footer with buttons with the new
    version of buttons from DS library
    
    ## How to test
    Check the specific page in the Example app to see how it changes.
    
    <img
    src="https://github.com/pagopa/io-app-design-system/assets/3959405/ac5f85b6-50d9-4001-94b8-7b604541199d"
    height="600"/>
    
    ---------
    
    Co-authored-by: Damiano Plebani <damiano.plebani@pagopa.it>

---
+++ src/components/layout/FooterWithButtons.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import { StyleSheet, View } from "react-native";
+import { IOStyles } from "../../core";
+import { BlockButtons, BlockButtonsProps } from "./BlockButtons";
+
+// TODO: Refactor with an unique component like `FooterTopShadow` after bonus vacanze
+const styles = StyleSheet.create({
+  container: {
+    overflow: "hidden",
+    width: "100%",
+    // This Magic number is an heritage of the app code-base, this component should be removed in favor of `GradientBottomAction`
+    marginTop: -50,
+    paddingTop: 50,
+    position: "absolute",
+    bottom: 0
+  }
+});
+
+/**
+ * Implements a component that show buttons as sticky footer
+ * It can include 1, 2 or 3 buttons. If they are 2, they can have the inlineHalf  or the inlineOneThird style
+ */
+export const FooterWithButtons = (props: BlockButtonsProps) => (
+  <View
+    style={styles.container}
+    accessible={props.accessible}
+    pointerEvents={"box-none"}
+    testID="FooterWithButtons"
+  >
+    <View style={IOStyles.footer}>
+      <BlockButtons {...props} />
+    </View>
+  </View>
+);
+
+export default FooterWithButtons;
