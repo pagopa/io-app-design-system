cde989d08769f51aee6ad8cd50e648b32d367a3a
Cristiano Tofani <cri.tofani@gmail.com>
2023-11-09 16:26:39 +0100

    fixes

--- src/stories/accordion/RawAccordion.stories.tsx
+++ src/stories/accordion/RawAccordion.stories.tsx
@@ -8,7 +8,7 @@ const meta = {
   component: AccordionItem,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "centered"
+    layout: "padded"
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"]
