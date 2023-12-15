be0f95066cb37d952e6f4c3c8b7e7c8a8beab7f5
Stefano Frontini <stefanofrontini75@gmail.com>
2023-12-15 09:49:23 +0100

    [IOPLT-181] - snapshots experimental (#168)
    ## Short description
    Adds snapshots tests for the new design system variant.
    
    ## List of changes proposed in this pull request
    Now snapshots tests cover also the new variant as well as legacy variant
    
    ## How to test
    yarn test
    
    ---------
    
    Co-authored-by: Cristiano Tofani <cri.tofani@gmail.com>

--- src/components/banner/__test__/banner.test.tsx
+++ src/components/banner/__test__/banner.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Alert, View } from "react-native";
 import * as TestRenderer from "react-test-renderer";
+import { TestRendererWithExperimentalEnabledContextProvider } from "../../../utils/testing";
 import { Banner } from "../Banner";
 
 const viewRef = React.createRef<View>();
@@ -11,6 +12,23 @@ const onLinkPress = () => {
 describe("Test Banner Components", () => {
   it("Banner Snapshot", () => {
     const advice = TestRenderer.create(
+      <Banner
+        viewRef={viewRef}
+        color="neutral"
+        size="big"
+        title="Banner title"
+        pictogramName="charity"
+        action="Action text"
+        onPress={onLinkPress}
+      />
+    ).toJSON();
+    expect(advice).toMatchSnapshot();
+  });
+});
+
+describe("Test Banner Components - Experimental Enabled", () => {
+  it("Banner Snapshot", () => {
+    const advice = TestRendererWithExperimentalEnabledContextProvider(
       <Banner
         viewRef={viewRef}
         color="neutral"
