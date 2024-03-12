94b51c32739f1b38cd2aaf1ec8e574bb6ed7b1e4
Cristiano Tofani <cri.tofani@gmail.com>
2024-03-12 17:30:53 +0100

    chore: NumberPad legacy colors (#214)
    ## Short description
    This PR adds NumberPad legacy colors to support io-app migration
    
    ## List of changes proposed in this pull request
    - NumberButton
    - BaseTypography
    
    ## How to test
    Check the related page on example app
    
    ---------
    
    Co-authored-by: Damiano Plebani <damiano.plebani@pagopa.it>

--- example/src/pages/NumberPad.tsx
+++ example/src/pages/NumberPad.tsx
@@ -1,18 +1,20 @@
-import * as React from "react";
-import { Alert, View } from "react-native";
 import {
+  CodeInput,
+  ContentWrapper,
   H1,
   H5,
-  IOVisualCostants,
-  IOStyles,
-  VSpacer,
-  NumberPad,
-  CodeInput,
-  ListItemSwitch,
   IOColors,
-  LabelSmallAlt
+  IOStyles,
+  IOVisualCostants,
+  LabelSmallAlt,
+  ListItemSwitch,
+  NumberPad,
+  VSpacer,
+  useIOExperimentalDesign
 } from "@pagopa/io-app-design-system";
 import { useNavigation } from "@react-navigation/native";
+import * as React from "react";
+import { Alert, View } from "react-native";
 
 const PIN_LENGTH = 6;
 /**
@@ -22,6 +24,12 @@ const PIN_LENGTH = 6;
 export const NumberPadScreen = () => {
   const [value, setValue] = React.useState("");
   const [blueBackground, setBlueBackground] = React.useState(false);
+  const { isExperimental } = useIOExperimentalDesign();
+
+  const primaryBackground = React.useMemo(
+    () => (isExperimental ? IOColors["blueIO-500"] : IOColors.blue),
+    [isExperimental]
+  );
 
   const navigation = useNavigation();
 
@@ -34,54 +42,52 @@ export const NumberPadScreen = () => {
   React.useEffect(() => {
     navigation.setOptions({
       headerStyle: {
-        backgroundColor: blueBackground
-          ? IOColors["blueIO-500"]
-          : IOColors.white
+        backgroundColor: blueBackground ? primaryBackground : IOColors.white
       }
     });
-  }, [blueBackground, navigation]);
+  }, [blueBackground, primaryBackground, navigation]);
   return (
     <View
       style={{
         flexGrow: 1,
         paddingVertical: IOVisualCostants.appMarginDefault,
-        backgroundColor: blueBackground
-          ? IOColors["blueIO-500"]
-          : IOColors.white
+        backgroundColor: blueBackground ? primaryBackground : IOColors.white
       }}
     >
-      <ListItemSwitch
-        label="Attiva sfondo blu"
-        value={blueBackground}
-        onSwitchValueChange={() => setBlueBackground(v => !v)}
-      />
-      <View style={IOStyles.alignCenter}>
-        <H1>NumberPad + Code Input</H1>
-        <H5>{"Value Typed on the NumberPad component"}</H5>
-        <VSpacer />
+      <ContentWrapper>
+        <ListItemSwitch
+          label="Attiva sfondo blu"
+          value={blueBackground}
+          onSwitchValueChange={() => setBlueBackground(v => !v)}
+        />
+        <View style={IOStyles.alignCenter}>
+          <H1>NumberPad + Code Input</H1>
+          <H5>{"Value Typed on the NumberPad component"}</H5>
+          <VSpacer />
 
-        <LabelSmallAlt color={blueBackground ? "white" : "black"}>
-          {value}
-        </LabelSmallAlt>
-      </View>
-      <VSpacer />
-      <CodeInput
-        value={value}
-        length={PIN_LENGTH}
-        variant={blueBackground ? "light" : "dark"}
-        onValueChange={onValueChange}
-        onValidate={v => v === "123456"}
-      />
-      <VSpacer size={48} />
-      <NumberPad
-        value={value}
-        deleteAccessibilityLabel="Delete"
-        onValueChange={onValueChange}
-        variant={blueBackground ? "dark" : "light"}
-        biometricType="FACE_ID"
-        biometricAccessibilityLabel="Face ID"
-        onBiometricPress={() => Alert.alert("biometric")}
-      />
+          <LabelSmallAlt color={blueBackground ? "white" : "black"}>
+            {value}
+          </LabelSmallAlt>
+        </View>
+        <VSpacer />
+        <CodeInput
+          value={value}
+          length={PIN_LENGTH}
+          variant={blueBackground ? "light" : "dark"}
+          onValueChange={onValueChange}
+          onValidate={v => v === "123456"}
+        />
+        <VSpacer size={48} />
+        <NumberPad
+          value={value}
+          deleteAccessibilityLabel="Delete"
+          onValueChange={onValueChange}
+          variant={blueBackground ? "dark" : "light"}
+          biometricType="FACE_ID"
+          biometricAccessibilityLabel="Face ID"
+          onBiometricPress={() => Alert.alert("biometric")}
+        />
+      </ContentWrapper>
     </View>
   );
 };
