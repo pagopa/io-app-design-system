0ba833caca152d06d57e648c7c32726ef5c5177a
Federico Mastrini <federicomastrini93@gmail.com>
2024-03-29 12:22:09 +0100

    [SIW-965] Add `ModuleCredential` component (#233)
    ## Short description
    This PR adds the `ModuleCredential` component
    
    ## List of changes proposed in this pull request
    - Added `ModuleCredential` component
    - Added examples
    
    ## How to test
    Navigate to Modules > ModuleCredential, check that the component is
    rendere correctly, like in the preview screenshot.
    
    ## Preview
    
    <img width="250" alt="Screenshot 2024-03-29 alle 11 52 05"
    src="https://github.com/pagopa/io-app-design-system/assets/6160324/5563fad6-8d40-4831-bf89-b0b2ac4e6135">
    
    ---------
    
    Co-authored-by: Damiano Plebani <damiano.plebani@pagopa.it>

---
+++ src/components/modules/ModuleCredential.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  ImageURISource,
+  StyleSheet,
+  View
+} from "react-native";
+import Placeholder from "rn-placeholder";
+import { WithTestID } from "src/utils/types";
+import {
+  IOListItemVisualParams,
+  IOModuleStyles,
+  IOSelectionListItemVisualParams,
+  IOStyles,
+  useIOTheme
+} from "../../core";
+import { Badge } from "../badge";
+import { IOIcons, Icon } from "../icons";
+import { HSpacer } from "../spacer";
+import { LabelSmallAlt } from "../typography";
+import {
+  PressableModuleBase,
+  PressableModuleBaseProps
+} from "./PressableModuleBase";
+
+type LoadingProps = {
+  isLoading: true;
+};
+
+type ImageProps =
+  | { icon: IOIcons; image?: never }
+  | { icon?: never; image: ImageURISource | ImageSourcePropType };
+
+type BaseProps = {
+  isLoading?: false;
+  label: string;
+  badge?: Badge;
+} & ImageProps &
+  PressableModuleBaseProps;
+
+type ModuleCredentialProps = LoadingProps | BaseProps;
+
+const ModuleCredential = (props: WithTestID<ModuleCredentialProps>) => {
+  const theme = useIOTheme();
+
+  if (props.isLoading) {
+    return <ModuleCredentialSkeleton />;
+  }
+
+  const { icon, image, label, onPress, badge, ...pressableProps } = props;
+
+  const iconComponent = (
+    <>
+      {icon && <Icon name={icon} size={24} color="grey-300" />}
+      {image && (
+        <Image
+          source={image}
+          style={styles.image}
+          accessibilityIgnoresInvertColors={true}
+        />
+      )}
+    </>
+  );
+
+  return (
+    <PressableModuleBase {...pressableProps} onPress={onPress}>
+      <View>{iconComponent}</View>
+      <View style={{ flexGrow: 1, flexShrink: 1, paddingHorizontal: 8 }}>
+        <LabelSmallAlt
+          color="blueIO-500"
+          numberOfLines={2}
+          lineBreakMode="middle"
+        >
+          {label}
+        </LabelSmallAlt>
+      </View>
+      <View style={IOStyles.row}>
+        {badge && <Badge {...badge} />}
+        {onPress && (
+          <Icon
+            name="chevronRightListItem"
+            color={theme["interactiveElem-default"]}
+            size={IOListItemVisualParams.chevronSize}
+          />
+        )}
+      </View>
+    </PressableModuleBase>
+  );
+};
+
+const ModuleCredentialSkeleton = () => (
+  <View style={IOModuleStyles.button}>
+    <View style={[IOStyles.row, IOStyles.alignCenter]}>
+      <Placeholder.Box animate="fade" width={24} height={24} radius={8} />
+      <HSpacer size={8} />
+      <Placeholder.Box animate="fade" width={96} height={19} radius={8} />
+    </View>
+    <Placeholder.Box animate="fade" width={64} height={22} radius={8} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  image: {
+    width: IOSelectionListItemVisualParams.iconSize,
+    height: IOSelectionListItemVisualParams.iconSize,
+    resizeMode: "contain"
+  }
+});
+
+export { ModuleCredential };
