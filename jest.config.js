e5f71d2ac3c1cad5a5857e8780431b59b549c6af
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2024-06-26 14:17:44 +0200

    add prepare script

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line functional/immutable-data
 module.exports = {
   preset: "react-native",
+  modulePathIgnorePatterns: [
+    "<rootDir>/example/node_modules",
+    "<rootDir>/lib/"
+  ],
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?@react-native|react-native|react-navigation|@react-navigation|react-navigation-redux-helpers|react-native-device-info|native-base|native-base-shoutem-theme|@shoutem/animation|@shoutem/ui|rn-placeholder|jsbarcode|@pagopa/react-native-cie|react-native-share|jail-monkey|@react-native-community/art|@react-native-community/push-notification-ios|@react-native-community/cameraroll|@codler|@react-native-community/datetimepicker|remark|unified|bail|is-plain-obj|trough|vfile|unist-util-stringify-position|mdast-util-from-markdown|mdast-util-to-string|micromark|parse-entities|character-entities|mdast-util-to-markdown|zwitch|longest-streak|io-react-native-zendesk|rn-qr-generator)"
   ],
@@ -12,5 +16,5 @@ module.exports = {
     "@testing-library/jest-native/extend-expect",
     "./jestSetupAfterEnv.js"
   ],
-  collectCoverage: true,
+  collectCoverage: true
 };
