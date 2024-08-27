5e2fa77233cdf9c5be7ebdac32e69dfe6ffb947d
Damiano Plebani <damiano.plebani@pagopa.it>
2024-08-27 12:02:59 +0200

    Exclude `Web` platform from the `useBoldTextEnabled` hook

--- src/utils/accessibility.ts
+++ src/utils/accessibility.ts
@@ -2,7 +2,7 @@ import { pipe } from "fp-ts/lib/function";
 import * as O from "fp-ts/lib/Option";
 import I18n from "i18n-js";
 import { useEffect, useState } from "react";
-import { AccessibilityInfo } from "react-native";
+import { AccessibilityInfo, Platform } from "react-native";
 
 /**
  * This function is used to get the text that will be read by the screen reader
@@ -26,22 +26,25 @@ export const useBoldTextEnabled = () => {
   const [boldTextEnabled, setBoldTextEnabled] = useState(false);
 
   useEffect(() => {
-    const boldTextChangedSubscription = AccessibilityInfo.addEventListener(
-      "boldTextChanged",
-      isBoldTextEnabled => {
-        setBoldTextEnabled(isBoldTextEnabled);
-      }
-    );
+    if (Platform.OS !== "web") {
+      const boldTextChangedSubscription = AccessibilityInfo.addEventListener(
+        "boldTextChanged",
+        isBoldTextEnabled => {
+          setBoldTextEnabled(isBoldTextEnabled);
+        }
+      );
 
-    AccessibilityInfo.isBoldTextEnabled()
-      .then(isBoldTextEnabled => {
-        setBoldTextEnabled(isBoldTextEnabled);
-      })
-      .catch(_ => false);
+      AccessibilityInfo.isBoldTextEnabled()
+        .then(isBoldTextEnabled => {
+          setBoldTextEnabled(isBoldTextEnabled);
+        })
+        .catch(_ => false);
 
-    return () => {
-      boldTextChangedSubscription.remove();
-    };
+      return () => {
+        boldTextChangedSubscription.remove();
+      };
+    }
+    return;
   }, []);
 
   return boldTextEnabled;
