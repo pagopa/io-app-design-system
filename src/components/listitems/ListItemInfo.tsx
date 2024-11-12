62a8bf83c6f7e188e5f61713420ac57fdbee95e1
Federico Mastrini <federicomastrini93@gmail.com>
2024-11-12 15:35:28 +0100

    chore: add `accessibiltyRole`

--- src/components/listitems/ListItemInfo.tsx
+++ src/components/listitems/ListItemInfo.tsx
@@ -1,5 +1,5 @@
 import React, { ComponentProps, useCallback, useMemo } from "react";
-import { Platform, View } from "react-native";
+import { AccessibilityRole, Platform, View } from "react-native";
 import {
   IOListItemStyles,
   IOListItemVisualParams,
@@ -7,12 +7,12 @@ import {
   useIOTheme
 } from "../../core";
 import { WithTestID } from "../../utils/types";
-import { IOIconSizeScale, IOIcons, Icon } from "../icons";
-import { H6, LabelSmall } from "../typography";
-import { ButtonLink, IconButton } from "../buttons";
 import { Badge } from "../badge";
+import { ButtonLink, IconButton } from "../buttons";
 import { LogoPaymentWithFallback } from "../common/LogoPaymentWithFallback";
+import { IOIconSizeScale, IOIcons, Icon } from "../icons";
 import { IOLogoPaymentType } from "../logos";
+import { H6, LabelSmall } from "../typography";
 
 type ButtonLinkActionProps = {
   type: "buttonLink";
@@ -41,6 +41,7 @@ export type ListItemInfo = WithTestID<{
   endElement?: EndElementProps;
   // Accessibility
   accessibilityLabel?: string;
+  accessibilityRole?: AccessibilityRole;
 }> &
   (
     | {
@@ -63,6 +64,7 @@ export const ListItemInfo = ({
   paymentLogoIcon,
   endElement,
   accessibilityLabel,
+  accessibilityRole,
   testID
 }: ListItemInfo) => {
   const theme = useIOTheme();
@@ -136,6 +138,7 @@ export const ListItemInfo = ({
       testID={testID}
       accessible={endElement === undefined ? true : false}
       accessibilityLabel={listItemAccessibilityLabel}
+      accessibilityRole={accessibilityRole}
     >
       <View style={IOListItemStyles.listItemInner}>
         {icon && (
