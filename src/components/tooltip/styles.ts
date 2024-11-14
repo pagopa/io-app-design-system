acab7359fc9dcea2fb86f03e38e3ec778d9aa208
Cristian Matteu <94987118+ChrisMattew@users.noreply.github.com>
2024-11-14 17:49:51 +0100

    [IOPID-2426] Tooltip component (#351)
    ## Short description
    This PR adds the `Tooltip` component
    
    ## List of changes proposed in this pull request
    - Created `Tooltip` component
    - Added `Tooltips` screen in the Example app to show the `Tooltip`
    behaviors
    ## Demo
    |iOS|Android|
    |----|-------|
    |<video
    src="https://github.com/user-attachments/assets/1060426b-0ec6-4340-b029-6fcdf07cfe10"></video>|<video
    src="https://github.com/user-attachments/assets/8a76daf8-687c-498e-a54d-ca161fb4a5d0"></video>|
    
    |A11Y iOS|A11Y Android|
    |----|-------|
    |<video
    src="https://github.com/user-attachments/assets/a03ee6be-5575-4634-a18a-35079ffa9f08"></video>|<video
    src="https://github.com/user-attachments/assets/2043f05a-6543-42f5-be14-cb091362e87f"></video>|
    
    >[!Important]
    > The `left` and `right` positions are calculated after the component is
    rendered. This implementation causes a minor glitch where the Tooltip
    repositions itself to achieve a centered alignment relative to the
    enclosing element.
    > To avoid this behavior, the opacity value of the component depends on
    the parameters returned by the `onLayout` prop. Once the `tooltipLayout`
    is defined, the opacity is set to a value of 1.
    
    ## How to test
    Run the example app and test the `Tooltip` different implementations
    
    ---------
    
    Co-authored-by: Damiano Plebani <damiano.plebani@pagopa.it>
    Co-authored-by: Alice Di Rico <83651704+Ladirico@users.noreply.github.com>

---
+++ src/components/tooltip/styles.ts
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import { IOColors } from '../../core';
+import { ChildrenCoords } from './utils/types';
+
+export const tooltipStyles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    backgroundColor: IOColors["grey-850"],
+    opacity: 0.6,
+    zIndex: 997
+  },
+  childrenContainer: {
+    position: "absolute",
+    zIndex: 1000
+  },
+  tooltipContainer: {
+    position: "absolute",
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    backgroundColor: IOColors.white,
+    borderRadius: 8,
+    zIndex: 2000,
+    overflow: "visible"
+  },
+  arrowContainer: {
+    position: "absolute",
+    display: 'flex',
+    zIndex: 3000
+  },
+  closeIcon: {
+    position: 'absolute',
+    right: 8,
+    top: 9 // It's been used `9` instead of `8` to fix accessibility focus order. In this way title is read before close icon.
+  }
+});
+
+export const getChildrenPosition = (childrenCoords: ChildrenCoords) => ({
+  top: childrenCoords.y,
+  left: childrenCoords.x,
+  width: childrenCoords.width,
+  height: childrenCoords.height
+});
