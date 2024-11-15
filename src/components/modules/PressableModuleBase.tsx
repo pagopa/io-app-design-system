e5c55264ca1e1b06ceb239cc211d1046b9dcbfa3
Damiano Plebani <damiano.plebani@pagopa.it>
2024-11-15 11:48:07 +0100

    Replace `useModuleSpringAnimation` with `useScaleAnimation`

--- src/components/modules/PressableModuleBase.tsx
+++ src/components/modules/PressableModuleBase.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { PropsWithChildren } from "react";
 import { Pressable } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, { useReducedMotion } from "react-native-reanimated";
+import { useScaleAnimation } from "../../utils/hooks";
 import {
   IOColors,
   IOModuleIDPSavedVSpacing,
@@ -9,7 +10,6 @@ import {
   useIOTheme
 } from "../../core";
 import { WithTestID } from "../../utils/types";
-import { useModuleSpringAnimation } from "./hooks/useModuleSpringAnimation";
 
 export type PressableModuleBaseProps = WithTestID<
   {
@@ -29,17 +29,24 @@ export const PressableModuleBase = ({
   children
 }: PropsWithChildren<PressableModuleBaseProps>) => {
   const theme = useIOTheme();
-  const { handlePressIn, handlePressOut, animatedStyle } =
-    useModuleSpringAnimation();
+  const reducedMotion = useReducedMotion();
+  const { onPressIn, onPressOut, scaleAnimationStyle } = useScaleAnimation(
+    reducedMotion ? "slight" : "medium"
+  );
+  /* We use a slight scaleEffect if `reducedMotion` is enabled.
+  We don't disable it completely because that's the only
+  difference between the two states "default" and "pressed".
+  If we remove it, they they won't be able to understand
+  if there's an ongoing interaction. */
 
   return (
     <Pressable
       onPress={onPress}
       testID={testID}
       accessible={true}
-      onPressIn={handlePressIn}
-      onPressOut={handlePressOut}
-      onTouchEnd={handlePressOut}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      onTouchEnd={onPressOut}
       accessibilityLabel={accessibilityLabel}
       accessibilityHint={accessibilityHint}
       accessibilityRole="button"
@@ -49,7 +56,7 @@ export const PressableModuleBase = ({
           IOModuleStyles.button,
           { borderColor: IOColors[theme["cardBorder-default"]] },
           withLooseSpacing && { paddingVertical: IOModuleIDPSavedVSpacing },
-          animatedStyle
+          scaleAnimationStyle
         ]}
       >
         {children}
