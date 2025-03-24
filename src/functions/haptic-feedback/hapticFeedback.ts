5effc66d7b3960a13e58f84d7f4df24432345b9f
Damiano Plebani <damiano.plebani@pagopa.it>
2025-03-24 15:01:38 +0100

    Update files to resolve TS errors

--- src/functions/haptic-feedback/hapticFeedback.ts
+++ src/functions/haptic-feedback/hapticFeedback.ts
@@ -4,10 +4,15 @@ import {
   HapticOptions
 } from "react-native-haptic-feedback";
 
+const defaultOptions: HapticOptions = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false
+};
+
 const triggerHaptic = (
-  type: string | HapticFeedbackTypes,
+  type: HapticFeedbackTypes | keyof typeof HapticFeedbackTypes,
   options?: HapticOptions
-) => trigger(type, options);
+) => trigger(type, { ...defaultOptions, ...options });
 
 export { triggerHaptic, HapticFeedbackTypes as HapticTypes };
 export type { HapticOptions };
