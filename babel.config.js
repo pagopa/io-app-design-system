2c3a9b03b716e53ee0222ba8f4a787cdf77a7fed
Damiano Plebani <damiano.plebani@pagopa.it>
2025-03-24 18:28:19 +0100

    Update `jest` setup

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
+    "@babel/plugin-transform-private-methods",
+    "@babel/plugin-transform-class-properties",
     [
       "react-native-reanimated/plugin",
       {
         globals: ["__scanCodes"]
       }
-    ]
+    ],
   ]
 };
