ee6dfae7d9036407ad1759585e11fbab1446003f
Cristiano Tofani <cristiano.tofani@pagopa.it>
2025-05-22 10:12:48 +0200

    memoize color maps

--- src/components/buttons/IOButton/styles.tsx
+++ src/components/buttons/IOButton/styles.tsx
@@ -4,6 +4,7 @@ import {
   useAnimatedProps,
   useAnimatedStyle
 } from "react-native-reanimated";
+import { useMemo } from "react";
 import { IOColors, hexToRgba, useIOTheme } from "../../../core";
 import { IOButtonColor, IOButtonProps, IOButtonVariant } from "./IOButton";
 
@@ -26,139 +27,158 @@ export const useButtonColorMap = (variant: IOButtonVariant) => {
   const mapColorStatesVariantSolid: Record<
     NonNullable<IOButtonProps["color"]>,
     ColorStates
-  > = {
-    // Primary
-    primary: {
-      background: {
-        default: IOColors[theme["interactiveElem-default"]],
-        pressed: IOColors[theme["interactiveElem-pressed"]],
-        disabled: IOColors[theme["interactiveElem-disabled"]]
+  > = useMemo(
+    () => ({
+      // Primary
+      primary: {
+        background: {
+          default: IOColors[theme["interactiveElem-default"]],
+          pressed: IOColors[theme["interactiveElem-pressed"]],
+          disabled: IOColors[theme["interactiveElem-disabled"]]
+        },
+        foreground: {
+          default: IOColors[theme["buttonText-default"]],
+          pressed: IOColors[theme["buttonText-default"]],
+          disabled: IOColors[theme["buttonText-disabled"]]
+        }
       },
-      foreground: {
-        default: IOColors[theme["buttonText-default"]],
-        pressed: IOColors[theme["buttonText-default"]],
-        disabled: IOColors[theme["buttonText-disabled"]]
-      }
-    },
-    // Danger
-    danger: {
-      background: {
-        default: IOColors["error-600"],
-        pressed: IOColors["error-500"],
-        disabled: IOColors[theme["interactiveElem-disabled"]]
+      // Danger
+      danger: {
+        background: {
+          default: IOColors["error-600"],
+          pressed: IOColors["error-500"],
+          disabled: IOColors[theme["interactiveElem-disabled"]]
+        },
+        foreground: {
+          default: IOColors[theme["buttonText-danger"]],
+          pressed: IOColors[theme["buttonText-danger"]],
+          disabled: IOColors[theme["buttonText-disabled"]]
+        }
       },
-      foreground: {
-        default: IOColors[theme["buttonText-danger"]],
-        pressed: IOColors[theme["buttonText-danger"]],
-        disabled: IOColors[theme["buttonText-disabled"]]
+      // Contrast
+      contrast: {
+        background: {
+          default: IOColors.white,
+          pressed: IOColors["blueIO-50"],
+          disabled: IOColors["blueIO-50"]
+        },
+        foreground: {
+          default: IOColors["blueIO-500"],
+          pressed: IOColors["blueIO-500"],
+          disabled: IOColors["blueIO-500"]
+        }
       }
-    },
-    // Contrast
-    contrast: {
-      background: {
-        default: IOColors.white,
-        pressed: IOColors["blueIO-50"],
-        disabled: IOColors["blueIO-50"]
-      },
-      foreground: {
-        default: IOColors["blueIO-500"],
-        pressed: IOColors["blueIO-500"],
-        disabled: IOColors["blueIO-500"]
-      }
-    }
-  };
+    }),
+    [theme]
+  );
 
   const mapColorStatesVariantOutline: Record<
     NonNullable<IOButtonProps["color"]>,
     ColorStates
-  > = {
-    // Primary
-    primary: {
-      background: {
-        default: hexToRgba(IOColors[theme["interactiveElem-pressed"]], 0),
-        pressed: hexToRgba(IOColors[theme["interactiveElem-pressed"]], 0.1),
-        disabled: "transparent"
+  > = useMemo(
+    () => ({
+      // Primary
+      primary: {
+        background: {
+          default: hexToRgba(IOColors[theme["interactiveElem-pressed"]], 0),
+          pressed: hexToRgba(IOColors[theme["interactiveElem-pressed"]], 0.1),
+          disabled: "transparent"
+        },
+        foreground: {
+          default: IOColors[theme["interactiveElem-default"]],
+          pressed: IOColors[theme["interactiveElem-pressed"]],
+          disabled: IOColors[theme["interactiveOutline-disabled"]]
+        }
       },
-      foreground: {
-        default: IOColors[theme["interactiveElem-default"]],
-        pressed: IOColors[theme["interactiveElem-pressed"]],
-        disabled: IOColors[theme["interactiveOutline-disabled"]]
-      }
-    },
-    // Danger
-    danger: {
-      background: {
-        default: hexToRgba(IOColors["error-600"], 0),
-        pressed: hexToRgba(IOColors["error-600"], 0.1),
-        disabled: "transparent"
+      // Danger
+      danger: {
+        background: {
+          default: hexToRgba(IOColors["error-600"], 0),
+          pressed: hexToRgba(IOColors["error-600"], 0.1),
+          disabled: "transparent"
+        },
+        foreground: {
+          default: IOColors[theme.errorText],
+          pressed: IOColors[theme.errorText],
+          disabled: IOColors[theme["buttonText-disabled"]]
+        }
       },
-      foreground: {
-        default: IOColors[theme.errorText],
-        pressed: IOColors[theme.errorText],
-        disabled: IOColors[theme["buttonText-disabled"]]
+      // Contrast
+      contrast: {
+        background: {
+          default: hexToRgba(IOColors["blueIO-600"], 0),
+          pressed: hexToRgba(IOColors["blueIO-600"], 0.5),
+          disabled: "transparent"
+        },
+        foreground: {
+          default: IOColors.white,
+          pressed: IOColors.white,
+          disabled: IOColors["blueIO-200"]
+        }
       }
-    },
-    // Contrast
-    contrast: {
-      background: {
-        default: hexToRgba(IOColors["blueIO-600"], 0),
-        pressed: hexToRgba(IOColors["blueIO-600"], 0.5),
-        disabled: "transparent"
-      },
-      foreground: {
-        default: IOColors.white,
-        pressed: IOColors.white,
-        disabled: IOColors["blueIO-200"]
-      }
-    }
-  };
+    }),
+    [theme]
+  );
 
-  const transparentLinkBackground: ColorStates["background"] = {
-    default: "transparent",
-    pressed: "transparent",
-    disabled: "transparent"
-  };
+  const transparentLinkBackground: ColorStates["background"] = useMemo(
+    () => ({
+      default: "transparent",
+      pressed: "transparent",
+      disabled: "transparent"
+    }),
+    []
+  );
 
   const mapColorStatesVariantLink: Record<
     NonNullable<IOButtonProps["color"]>,
     ColorStates
-  > = {
-    // Primary
-    primary: {
-      foreground: {
-        default: IOColors[theme["interactiveElem-default"]],
-        pressed: IOColors[theme["interactiveElem-pressed"]],
-        disabled: hexToRgba(IOColors[theme["interactiveElem-default"]], 0.85)
+  > = useMemo(
+    () => ({
+      // Primary
+      primary: {
+        foreground: {
+          default: IOColors[theme["interactiveElem-default"]],
+          pressed: IOColors[theme["interactiveElem-pressed"]],
+          disabled: hexToRgba(IOColors[theme["interactiveElem-default"]], 0.85)
+        },
+        background: transparentLinkBackground
       },
-      background: transparentLinkBackground
-    },
-    // Danger
-    danger: {
-      foreground: {
-        default: IOColors[theme.errorText],
-        pressed: IOColors[theme.errorText],
-        disabled: hexToRgba(IOColors[theme.errorText], 0.85)
+      // Danger
+      danger: {
+        foreground: {
+          default: IOColors[theme.errorText],
+          pressed: IOColors[theme.errorText],
+          disabled: hexToRgba(IOColors[theme.errorText], 0.85)
+        },
+        background: transparentLinkBackground
       },
-      background: transparentLinkBackground
-    },
-    // Contrast
-    contrast: {
-      foreground: {
-        default: IOColors.white,
-        pressed: hexToRgba(IOColors.white, 0.85),
-        disabled: hexToRgba(IOColors.white, 0.5)
-      },
-      background: transparentLinkBackground
-    }
-  };
+      // Contrast
+      contrast: {
+        foreground: {
+          default: IOColors.white,
+          pressed: hexToRgba(IOColors.white, 0.85),
+          disabled: hexToRgba(IOColors.white, 0.5)
+        },
+        background: transparentLinkBackground
+      }
+    }),
+    [theme, transparentLinkBackground]
+  );
 
-  const colorMap = {
-    solid: mapColorStatesVariantSolid,
-    outline: mapColorStatesVariantOutline,
-    link: mapColorStatesVariantLink
-  };
+  const colorMap = useMemo(
+    () => ({
+      solid: mapColorStatesVariantSolid,
+      outline: mapColorStatesVariantOutline,
+      link: mapColorStatesVariantLink
+    }),
+    [
+      mapColorStatesVariantLink,
+      mapColorStatesVariantOutline,
+      mapColorStatesVariantSolid
+    ]
+  );
 
-  return colorMap[variant];
+  return useMemo(() => colorMap[variant], [colorMap, variant]);
 };
 
 export const useButtonAnimatedStyles = (
