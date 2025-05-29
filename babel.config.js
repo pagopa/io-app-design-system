cae803ecde4fd8b4f2916c5832f3877f9e4cd81b
Emanuele Dall'Ara <71103219+LeleDallas@users.noreply.github.com>
2025-05-29 11:29:05 +0200

    fix: jest setup

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ["module:@react-native/babel-preset"],
   plugins: [
-    "@babel/plugin-transform-private-methods",
-    "@babel/plugin-transform-class-properties",
+    ["@babel/plugin-transform-private-methods", { "loose": true }],
+    ["@babel/plugin-transform-class-properties", { "loose": true }],
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
     "react-native-reanimated/plugin"
   ]
 };
